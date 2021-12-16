525aaaa69759e0178c2c2f33555f67f2762adbb4
Andrew Cooper <github@cmalu.neomailbox.ch>
2021-12-16 23:20:49 +0000

    Automated upload.

--- upload_scripts.sh
+++ upload_scripts.sh
@@ -2,3 +2,7 @@
 
 rm file_list.txt
 tree -i -f | grep .js$ | sed 's/^\./https:\/\/raw\.githubusercontent\.com\/mikomyazaki\/bitburner\/main/g'
+
+git add .
+git commit -m "Automated upload."
+git push
