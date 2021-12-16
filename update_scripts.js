ad192be5c6731c18b69968ebbb85d5dd39342853
Andrew Cooper <github@cmalu.neomailbox.ch>
2021-12-16 23:44:51 +0000

    Automated upload.

--- update_scripts.js
+++ update_scripts.js
@@ -6,10 +6,13 @@ export async function main(ns) {
 	let urls = ns.read("file_list.txt");
 	let arr = urls.split("\n");
 
+	let output = ""
 	for (var i in arr) {
 		if(arr[i].length) {
 			await ns.wget(arr[i], arr[i].substring(prefix.length))
-			ns.tprint("Downloaded: ", arr[i].substring(prefix.length))
+			output += "Downloaded: " + arr[i].substring(prefix.length) + "\n";
 		}
 	}
+
+	ns.tprint(output);
 }
