fdbe6ed6bca67104b339b85b057461f465481f6a
Andrew Cooper <github@cmalu.neomailbox.ch>
2021-12-17 18:52:00 +0000

    Automated upload.

--- scripts/pserv_upgrader.js
+++ scripts/pserv_upgrader.js
@@ -16,8 +16,6 @@ async function getAffordableUpgradeLevel(ns, server, current_money) {
     return next_ram > current_ram ? next_ram : null
 }
 
-import { initialize_server } from '/utils/master_controller.js';
-
 /** @param {NS} ns **/
 export async function main(ns) {
     let empty_server_slots = ns.getPurchasedServerLimit() - ns.getPurchasedServers().length;
@@ -26,10 +24,10 @@ export async function main(ns) {
     for (let i = 0; i < pservs.length; i++) {
         let current_money = ns.getServerMoneyAvailable("home");
         let pserv = pservs[i].info.hostname;
-        let upgrade = getAffordableUpgrade(ns, pserv, current_money)
+        let upgrade = getAffordableUpgrade(ns, pserv, current_money);
 
         if (upgrade) {
-            await ns.killall(pserv)
+            await ns.killall(pserv);
             await ns.deleteServer(pserv);
             await ns.purchaseServer(pserv, upgrade);
 
@@ -37,10 +35,8 @@ export async function main(ns) {
 
             for (let j = 0; j < pservs[i].ps; j++) {
                 let script = pservs[i].ps.filename;
-            //    await ns.exec(script, pserv, Math.floor(ns.getServerMaxRam(pserv) / ns.getScriptRam(script)), ...pservs[i].ps.args);
+                await ns.exec(script, pserv, Math.floor(ns.getServerMaxRam(pserv) / ns.getScriptRam(script)), ...pservs[i].ps.args);
             }
         }
     }
-
-    await initialize_server(ns)
 }
