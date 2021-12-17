7f77d8c6f9b36aa2cfc13b59a908385e413d1f7b
Andrew Cooper <github@cmalu.neomailbox.ch>
2021-12-17 18:47:13 +0000

    Automated upload.

--- scripts/pserv_upgrader.js
+++ scripts/pserv_upgrader.js
@@ -11,33 +11,33 @@ export async function scan_pservs(ns) {
 
 async function getAffordableUpgradeLevel(ns, server, current_money) {
     let current_ram = ns.getServer(server).maxRam;
-    let next_ram =  function ram_limit(current_ram) { ns.getPurchasedServerCost(current_ram) > current_money ? return current_ram/2 : ram_limit(current_ram*2) }
+    let next_ram = function ram_limit(current_ram) { ns.getPurchasedServerCost(current_ram) > current_money ? return current_ram / 2 : ram_limit(current_ram * 2) }
 
     return next_ram > current_ram ? next_ram : null
 }
 
-import {initialize_server} from '/utils/master_controller.js';
+import { initialize_server } from '/utils/master_controller.js';
 
 /** @param {NS} ns **/
 export async function main(ns) {
     let empty_server_slots = ns.getPurchasedServerLimit() - ns.getPurchasedServers().length;
-   let pservs = scan_pservs(ns);
+    let pservs = scan_pservs(ns);
 
     for (let i = 0; i < pservs.length; i++) {
         let current_money = ns.getServerMoneyAvailable("home");
-        let upgrade = getAffordableUpgrade(ns, pservs[i].info.hostname, current_money)
+        let pserv = pservs[i].info.hostname;
+        let upgrade = getAffordableUpgrade(ns, pserv, current_money)
 
         if (upgrade) {
-            let pserv = pservs[i].info.hostname;
             await ns.killall(pserv)
             await ns.deleteServer(pserv);
             await ns.purchaseServer(pserv, upgrade);
 
             await initialize_server(ns, pserv);
-            
+
             for (let j = 0; j < pservs[i].ps; j++) {
                 let script = pservs[i].ps.filename;
-                await ns.exec(script, pserv, Math.floor(ns.getServerMaxRam(pserv) / ns.getScriptRam(script)), ...pservs[i].ps.args);
+            //    await ns.exec(script, pserv, Math.floor(ns.getServerMaxRam(pserv) / ns.getScriptRam(script)), ...pservs[i].ps.args);
             }
         }
     }
